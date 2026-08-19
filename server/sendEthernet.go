bc3b4ead8f6ffd3bda043d8981410a0309d75c25
Gerben Geijteman <gerben@hyperized.net>
2026-08-19 12:03:02 +0200

    server: move to the maintained gopacket fork
    google/gopacket has had no release since 2020 and little movement
    since; the community continuation at gopacket/gopacket is where fixes
    land now (v1.7.1, active this month). Drop-in import swap in the one
    file that uses it.

--- server/sendEthernet.go
+++ server/sendEthernet.go
@@ -12,8 +12,8 @@ import (
 	"net"
 	"syscall"
 
-	"github.com/google/gopacket"
-	"github.com/google/gopacket/layers"
+	"github.com/gopacket/gopacket"
+	"github.com/gopacket/gopacket/layers"
 	"github.com/insomniacslk/dhcp/dhcpv4"
 )
 
